b08f76450bcc3c1264732b7a0014f279649503d3
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2023-12-17 21:57:37 +0800

    added bing

--- navbar.php
+++ navbar.php
@@ -2,15 +2,16 @@
         <br><br><br><br><br><br><br>
         <h1>Menu</h1>
         <ul>
-            <!-- <li><a href="/">Home</a></li> -->
+            <li><a href="/">Home</a></li>
             <li><a href="conversations.view.php">Conversations</a></li>
             <li><a href="updates.view.php">Updates</a></li>
             <li class="dropdown">
                 <a href="#" class="dropbtn">A.I Companions</a>
                 <div class="dropdown-content">
-                    <a href="https://chat.openai.com/" target="_blank">ChatGPT</a>
-                    <a href="https://bard.google.com/chat" target="_blank">Bard</a>
-                    <a href="https://you.com/" target="_blank">You</a>
+                    <a href="https://chat.openai.com/" target="_blank">1. ChatGPT</a>
+                    <a href="https://bard.google.com/chat" target="_blank">2. Bard</a>
+                    <a href="https://you.com/" target="_blank">3. You</a>
+                    <a href="https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx" target="_blank">4. Bing AI</a>
                 </div>
             </li>
         </ul>
